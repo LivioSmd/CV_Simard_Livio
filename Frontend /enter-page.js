6babce6eb1e7376abc23609889606aec198f40ca
Livio Simard <liviosimard@yahoo.fr>
2023-07-20 01:52:10 +0200

    first commit

---
+++ Frontend /enter-page.js
@@ -0,0 +1,7 @@
+const boutonEntrer = document.querySelector(".glowing-btn")
+
+boutonEntrer.addEventListener("click", function() {
+    const target = "./index.html";
+
+    window.location.href = target;
+});
