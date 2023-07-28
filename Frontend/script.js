23db5dbca7e6077063547a08cb4571646dd7b90d
Livio Simard <liviosimard@yahoo.fr>
2023-07-28 23:22:29 +0200

    modifs modal

--- Frontend/script.js
+++ Frontend/script.js
@@ -36,16 +36,16 @@ function createImage(src, alt) {
 }
 
 // Fonction pour créer un bouton de projet avec événement de clic
-function createProjectButton(title, imagesSecondaires) {
+function createProjectButton(outils, epreuves, mission, date, title, imagesSecondaires) {
   const button = document.createElement('button');
   button.textContent = title;
   button.classList.add('card-btn'); // Ajouter la classe pour le style du bouton
-  button.addEventListener('click', () => openModal(title, imagesSecondaires));
+  button.addEventListener('click', () => openModal(outils, epreuves, mission, date, title, imagesSecondaires));
   return button;
 }
 
  // Fonction pour ouvrir la modale avec les images secondaires
-function openModal(title, imagesSecondaires) {
+function openModal(outils, epreuves, mission, date, title, imagesSecondaires) {
   const modalContent = document.getElementById('modalContent');
   modalContent.innerHTML = '';
 
@@ -53,10 +53,34 @@ function openModal(title, imagesSecondaires) {
   divImageTexte.classList.add('div-img-text');
   modalContent.appendChild(divImageTexte);
 
-  const textDescModale = document.createElement('div');
-  textDescModale.textContent = title;
-  textDescModale.classList.add('titre-desc-modale');
-  divImageTexte.appendChild(textDescModale);
+  const divTitle = document.createElement('div');
+  divTitle.classList.add('divTitle');
+  divImageTexte.appendChild(divTitle);
+
+  const titleDescModale = document.createElement('div');
+  titleDescModale.textContent = title;
+  titleDescModale.classList.add('titre-desc-modale');
+  divTitle.appendChild(titleDescModale);
+
+  const modalAnnee = document.createElement('p');
+  modalAnnee.innerHTML = "Année:" + ' ' + date;
+  modalAnnee.classList.add('titre-secondaire-modal');
+  titleDescModale.appendChild(modalAnnee);
+
+  const modalMission = document.createElement('p');
+  modalMission.innerHTML = "Mission:" + ' ' + mission;
+  modalMission.classList.add('titre-secondaire-modal');
+  titleDescModale.appendChild(modalMission);
+
+  const modalDifficultes = document.createElement('p');
+  modalDifficultes.innerHTML = "Difficultés:" + ' ' + epreuves;
+  modalDifficultes.classList.add('titre-secondaire-modal');
+  titleDescModale.appendChild(modalDifficultes);
+
+  const modalTechno = document.createElement('p');
+  modalTechno.innerHTML = "Technologies utilisées:" + ' ' + outils;
+  modalTechno.classList.add('titre-secondaire-modal');
+  titleDescModale.appendChild(modalTechno);
 
   imagesSecondaires.forEach(image => {
     const imageElement = createImage(image, 'Image Secondaire');
@@ -96,7 +120,7 @@ fetch('data.json')
       const divOverlay = document.createElement('div');
       divOverlay.classList.add('overlay');
 
-      const button = createProjectButton(project.titre, project.imagesSecondaires);
+      const button = createProjectButton(project.outils, project.epreuves, project.mission, project.date, project.titre, project.imagesSecondaires);
       projectElement.appendChild(divOverlay);
       divOverlay.appendChild(button);
       projectsContainer.appendChild(projectElement);
